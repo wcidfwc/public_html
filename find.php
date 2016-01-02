a6dba6e63a817e0e48a103e149254e0410171df4
zhuyifei1999 <zhuyifei1999@gmail.com>
2016-01-02 19:07:02 +0800

    [find] typo and grammar fixes

--- find.php
+++ find.php
@@ -12,7 +12,7 @@ start("What can I do for Wikimedia Commons?");
 <?php
 if ( isset( $_GET['new'] ) && !empty( $_GET['new'] ) ) {
 echo "<div class=\"alert alert-warning alert-dismissible fade in\" role=\"alert\"> <a href=\"find.php?nextid=0\" type=\"button\" class=\"close\" 
-aria-label=\"Close\"><span aria-hidden=\"true\">×</span></a> <strong>New at Wikimedia Commons?</strong> You probably schould read <a href=\"https://commons.wikimedia.org/wiki/Commons:Welcome\">Commons:Welcome</a> first! :-)</div>";
+aria-label=\"Close\"><span aria-hidden=\"true\">×</span></a> <strong>New at Wikimedia Commons?</strong> You should probably read <a href=\"https://commons.wikimedia.org/wiki/Commons:Welcome\">Commons:Welcome</a> first! :-)</div>";
 }
 ?>
       <div class="jumbotron">
@@ -25,7 +25,7 @@ aria-label=\"Close\"><span aria-hidden=\"true\">×</span></a> <strong>New at Wik
 	<small><div style = "float: left;"><a href="index.php">home</a> | <a href="javascript:history.back()">back</a></div> <div style="text-align: right;"><?php help("<span class=\"caret\"></span> "); ?><a href="find.php?help">help</a></div></small>
     <?php help("<br><div class=\"alert alert-info alert-dismissible fade in\" role=\"alert\"> <a href=\"find.php?nextid=0\" type=\"button\" class=\"close\" 
 aria-label=\"Close\"><span aria-hidden=\"true\">×</span></a> <strong>Help</strong><br>This pags shows random stuff/tasks which you can do for Wikimedia Commons.<br><br>
-    If you are looking for a general overwiev you can take a look at <a href=\"https://commons.wikimedia.org/wiki/Commons:Welcome\">Commons:Welcome</a> and <a href=\"https://commons.wikimedia.org/wiki/Commons:Community_portal\">Commons:Community portal</a>.</div>");
+    If you are looking for a general overview you can take a look at <a href=\"https://commons.wikimedia.org/wiki/Commons:Welcome\">Commons:Welcome</a> and <a href=\"https://commons.wikimedia.org/wiki/Commons:Community_portal\">Commons:Community portal</a>.</div>");
     echo "</div>"; 
     footer()
 ?>
