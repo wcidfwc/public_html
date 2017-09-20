eb1a61cbef76e185a1840b6d1bdbab92fb1e99da
Nahid Sultan <nahid.sultan@hotmail.com>
2017-09-20 23:04:51 +0600

    Update translations.php

--- translations.php
+++ translations.php
@@ -4,49 +4,49 @@ TRANSLATION FILE
 This file contais the transations for the tool.
 Please stick on the exact format.
 */
-$allanguages = "[[?uselang=en|en]]";
+$allanguages = "[[?uselang=bn|bn]]";
 
 if($_GET["lang"] == "xxx") {
   //translations for english (=source text)
 } else {
-  $i18nTlang = "en";
-  $i18nTlangname = "English";
-  $i18nTtitle = "What can I do for Wikimedia Commons?";
-  $i18nTfind = "Find a project for me!";
-  $i18nTwis = "What is Wikimedia Commons?";
-  $i18nTintro = "Uploading, editing, translating, reviewing files, create graphics, ...";
-  $i18nTmp1 = "Uploading";
-  $i18nTmp1txt = "Are you a good photographer or illustrator? Don't hesitate to [[https://commons.wikimedia.org/wiki/Commons:Uploading|upload your files]] (Wikimedia Commons accepts only [[https://commons.wikimedia.org/wiki/COM:L|free content]]).";
-  $i18nTmp2 = "Editing";
-  $i18nTmp2txt = "Help with [[https://commons.wikimedia.org/wiki/Commons:Categories|categorising]] pages. There is a huge backlog at [[https://commons.wikimedia.org/wiki/Category:Media_needing_categories|Category:Media needing categories]].";
-  $i18nTmp3 = "Maintenance";
-  $i18nTmp3txt = "There are a number of [[https://commons.wikimedia.org/wiki/Category:Commons_maintenance_content|maintenance backlogs]] on commons requiring attention.";
-  $i18nTmp4 = "Translating";
-  $i18nTmp4txt = "Wikimedia Commons is a [[https://commons.wikimedia.org/wiki/Commons:Language_policy|multilingual project]]. You can help with [[https://commons.wikimedia.org/wiki/Special:LanguageStats|translating pages]] into a language other than English. For example: Help translating the [[https://commons.wikimedia.org/wiki/Template:Potd|picture of the day's description]].";
-  $i18nTmp5 = "Vandalism";
-  $i18nTmp5txt = "Wikimedia Commons is a wiki which everyone can edit. Help identifying [[https://commons.wikimedia.org/wiki/COM:CVU|vandalism]] and detecting [[https://commons.wikimedia.org/wiki/Commons:How_to_detect_copyright_violations|copyright violations]].";
-  $i18nTmp6 = "Coding";
-  $i18nTmp6txt = "Help [[https://commons.wikimedia.org/wiki/Commons:User_scripts|developing]] user scripts &amp; tools for the community, or tackle some related [[https://phabricator.wikimedia.org/tag/commons/|MediaWiki bugs]].";
-  $i18nTsource = "View source / Edit";
-  $i18nTlegal = "Legal";
-  $i18nTlegal2 = "Not officially affiliated with the Wikimedia Foundation";
-  $i18nThome = "Home";
-  $i18nThomehelp = "This pags shows random stuff/tasks which you can do for Wikimedia Commons.<br><br> If you are looking for a general overview you can take a look at [[https://commons.wikimedia.org/wiki/Commons:Welcome|Commons:Welcome]] and [[https://commons.wikimedia.org/wiki/Commons:Community_portal|Commons:Community portal]].";
-  $i18nTback = "Back";
-  $i18nThelp = "Help";
-  $i18nThelp1 = "This pags shows random stuff/tasks which you can do for Wikimedia Commons.";
-  $i18nThelp2 = "If you are looking for a general overview you can take a look at [[https://commons.wikimedia.org/wiki/Commons:Welcome|Commons:Welcome]] and [[https://commons.wikimedia.org/wiki/Commons:Community_portal|Commons:Community portal]].";
-  $i18nTnewac = "<strong>New at Wikimedia Commons?</strong> You should probably read [[https://commons.wikimedia.org/wiki/Commons:Welcome|Commons:Welcome]] first! :-)";
+  $i18nTlang = "bn";
+  $i18nTlangname = "বাংলা";
+  $i18nTtitle = "উইকিমিডিয়া কমন্সের জন্য আমি কি করতে পারি?";
+  $i18nTfind = "আমার জন্য একটি প্রকল্প খুঁজে বের করুন!";
+  $i18nTwis = "উইকিমিডিয়া কমন্স কি?";
+  $i18nTintro = "আপলোড করা, সম্পাদনা, অনুবাদ, ফাইল পর্যালোচনা, গ্রাফিক্স তৈরি, ...";
+  $i18nTmp1 = "আপলোড করা";
+  $i18nTmp1txt = "আপনি কি ভালো আলোকচিত্ৰকর বা অঙ্কনশিল্পী? [[https://commons.wikimedia.org/wiki/Commons:Uploading|আপনার ছবি আপলোড]] করতে পারেন (উইকিমিডিয়া কমন্স শুধুমাত্র [[https://commons.wikimedia.org/wiki/COM:L|মুক্ত উপাদান গ্রহণ করে]])।";
+  $i18nTmp2 = "সম্পাদনা";
+  $i18nTmp2txt = "পাতা [[https://commons.wikimedia.org/wiki/Commons:Categories|বিষয়শ্রেণীভুক্ত]] করতে সাহায্য করুন। [[https://commons.wikimedia.org/wiki/Category:Media_needing_categories|যেসব মিডিয়াতে বিশষশ্রেণী প্রয়োজন]] পাতায় অনেক ব্যাকলগ রয়েছে।";
+  $i18nTmp3 = "রক্ষণাবেক্ষণ";
+  $i18nTmp3txt = "এখানে বেশ কিছু [[https://commons.wikimedia.org/wiki/Category:Commons_maintenance_content|রক্ষণাবেক্ষণ]] ব্যাকলগ রয়েছে যাতে দৃষ্টি দিতে হবে।";
+  $i18nTmp4 = "অনুবাদ";
+  $i18nTmp4txt = "উইকিমিডিয়া কমন্স একটি [[https://commons.wikimedia.org/wiki/Commons:Language_policy|বহুভাষা প্রকল্প]]। আপনি ইংরেজির বাইরে অন্য ভাষায় [[https://commons.wikimedia.org/wiki/Special:LanguageStats|পাতা অনুবাদ]] করে সাহায্য করতে পারেন। উদাহরণস্বরুপ: [[https://commons.wikimedia.org/wiki/Template:Potd|আজকের নির্বাচিত চিত্রের বর্ণনা]] অনুবাদ করুন।";
+  $i18nTmp5 = "ধ্বংসপ্রবণতা";
+  $i18nTmp5txt = "উইকিমিডিয়া কমন্স এমন একটি উইকি যেটা সবাই সম্পাদনা করতে পারেন। [[https://commons.wikimedia.org/wiki/COM:CVU|ধ্বংসপ্রবণা]] রোধ করতে এবং [[https://commons.wikimedia.org/wiki/Commons:How_to_detect_copyright_violations|কপিরাইট সমস্যা]] খুঁজে বের করতে সাহায্য করুন।";
+  $i18nTmp6 = "কোডিং";
+  $i18nTmp6txt = "ব্যবহারকারী স্ক্রিপ্ট [[https://commons.wikimedia.org/wiki/Commons:User_scripts|উন্নয়নে]] সাহায্য করুন &amp; সম্প্রদায়ের সরঞ্জাম, বা এ সম্পর্কিত কিছু [[https://phabricator.wikimedia.org/tag/commons/|মিডিয়াউইকি বাগ]]।";
+  $i18nTsource = "উৎস দেখুন / সম্পাদনা";
+  $i18nTlegal = "আইনী";
+  $i18nTlegal2 = "উইকিমিডিয়া ফাউন্ডেশনের সাথে অফিসিয়ালভাবে যুক্ত নয়";
+  $i18nThome = "প্রধান পাতা";
+  $i18nThomehelp = "এই পাতাটিতে অজানা অনেক পাতাই রয়েছে যাতে আপনার সাহায্য প্রয়োজন।<br><br> আপনি যদি কমন্স সম্পর্কে সাধারণ একটি ধারণা নিতে চান তাহলে [[https://commons.wikimedia.org/wiki/Commons:Welcome|কমন্স:স্বাগতম]] দেখুন এবং [[https://commons.wikimedia.org/wiki/Commons:Community_portal|কমন্স:সম্প্রদায় প্রবেশদ্বার]] দেখুন।";
+  $i18nTback = "পিছনে যান";
+  $i18nThelp = "সাহায্য";
+  $i18nThelp1 = "এই পাতাটিতে অজানা অনেক পাতাই রয়েছে যাতে আপনার সাহায্য প্রয়োজন।";
+  $i18nThelp2 = "আপনি যদি কমন্স সম্পর্কে সাধারণ একটি ধারণা নিতে চান তাহলে [[https://commons.wikimedia.org/wiki/Commons:Welcome|কমন্স:স্বাগতম]] দেখুন এবং [[https://commons.wikimedia.org/wiki/Commons:Community_portal|কমন্স:সম্প্রদায় প্রবেশদ্বার]] দেখুন।";
+  $i18nTnewac = "<strong>উইকিমিডিয়া কমন্সে নতুন??</strong> উইকিমিডিয়া কমন্সে নতুন?</strong> আপনার সম্ভবত প্রথমে [[https://commons.wikimedia.org/wiki/Commons:Welcome|কমন্স:স্বাগতম]] পাতাটি পড়া উচিত! :-)";
   $i18nTiol = "Languages";
 
   $i18nTwcid = array(
-    "Improve, clean up, or color images at the [[Commons:Graphic_Lab|Graphic Lab]]",
-    "[[Category:Categories_requiring_diffusion|Moving files into relevant subcategories]]",
-    "[[Commons:Featured_picture_candidates|Nominate or vote]] for Featured Pictures",
-    "[[Commons:QI|Nominate or vote]] for Quality images",
-    "[[Category:Media_needing_categories|Add]] files to categories",
-    "[[COM:Vandalism|Find and revert vandalism]] ([[Commons:Counter_Vandalism_Unit|Counter Vandalism Unit]])",
-    "Help with [[Special:LanguageStats|translating]] pages",
+    "[[Commons:Graphic_Lab|গ্রাফিক্স ল্যাবে]] ছবি পরিষ্কার, উন্নত বা বর্ণ পরিবর্তন করুন",
+    "[[Category:Categories_requiring_diffusion|ফাইল সম্পর্কিত বিষয়শ্রেণীতে স্থানান্তর করুন]]",
+    "নির্বাচিত চিত্র [[Commons:Featured_picture_candidates|মনোনয়ন বা ভোট]] দিন",
+    "মান সম্পন্ন চিত্র [[Commons:QI|মনোনয়ন বা ভোট]] দিন",
+    "ছবিতে বিষয়শ্রেণী [[Category:Media_needing_categories|যোগ]] করুন",
+    "[[COM:Vandalism|ধ্বংসপ্রবণতা খুঁজুন ও বাতিল করুন]] ([[Commons:Counter_Vandalism_Unit|ধ্বংসপ্রবণতা প্রতিরোধ ইউনিট]])",
+    "পাতা [[Special:LanguageStats|অনুবাদে]] সাহায্য করুন",
     "Help with the identification of [[Category:Unidentified_subjects|unidentified subjects]]",
     "Create [[Special:WantedCategories|wanted categories]]",
     "[[Special:UploadWizard|Upload your photographs]]",
